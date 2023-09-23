3c72c0620c715de94f3b9e984b03aa56d23ae489
Kshiro_AM <kshiro.am@gmail.com>
2023-09-23 17:32:30 +0800

    config submodule glad and glfw

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -2,13 +2,18 @@
 #include <Eigen/Core>
 
 #include <igl/readOFF.h>
+//#include <igl/opengl/glfw/Viewer.h>
+
 
 int main() {
 	Eigen::MatrixXd V;
 	Eigen::MatrixXi F;
 
 	igl::readOFF("asset/bumpy.off", V, F);
-	std::cout << V << std::endl;
+
+	//igl::opengl::glfw::Viewer viewer;
+	//viewer.data().set_mesh(V, F);
+	//viewer.launch();
 
 	return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -7,16 +7,22 @@ workspace "gmp"
         "Release"
     }
 
-    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-    IncludeDir = {}
-    IncludeDir["Eigen"] = "Application/vendor/eigen"
-    IncludeDir["libigl"] = "Application/vendor/libigl/include"
+IncludeDir = {}
+IncludeDir["Eigen"] = "Application/vendor/eigen"
+IncludeDir["libigl"] = "Application/vendor/libigl/include"
+IncludeDir["glad"] = "Application/vendor/glad/include"
+IncludeDir["GLFW"] = "Application/vendor/glfw/include"
+
+include "Application/vendor/glad"
+include "Application/vendor/glfw"
 
 project "Application"
     location "Application"
     kind "ConsoleApp"
     language "C++"
+    characterset "MBCS"
     
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -25,27 +31,30 @@ project "Application"
     {
         "%{prj.name}/src/**.cpp",
         "%{prj.name}/src/**.hpp",
-        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.h"
     }
 
     includedirs 
     {
-        IncludeDir["Eigen"],
-        IncludeDir["libigl"]
+        "%{IncludeDir.glad}",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.Eigen}",
+        "%{IncludeDir.libigl}"
     }
 
     links
     {
+        "glad",
+        "GLFW"
     }
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
-        
-        defines
-        {
-        }
+        staticruntime "On"
+
+    filter { "system:windows", "action:vs*" }        
+        buildoptions {"/bigobj"}
 
     filter "configurations:Debug"
         symbols "On"
